688c549feeac2a2f19de855da2570b9d8ce811f3
Violeta Saguier <50125418+VioletaSaguier@users.noreply.github.com>
2023-11-29 18:15:11 -0300

    Update app_tp2_mie6.py
    restore version

--- app_tp2_mie6.py
+++ app_tp2_mie6.py
@@ -6,19 +6,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Cargar el modelo TF-IDF y la matriz de similitud
 with open('modelo_recomendacion.pkl', 'rb') as file:
-    tfidf, cosine_sim, df = pickle.load(file)  # Asegúrate de que df también se incluya en el pickle
+    tfidf, cosine_sim = pickle.load(file)
 
 # Función para obtener recomendaciones para un nuevo usuario
-def get_recommendations(new_user_profile, tfidf_model, tfidf_matrix, cosine_sim):
+def get_recommendations(new_user_profile, tfidf_matrix, cosine_sim):
     # Transformar el perfil del nuevo usuario a un vector TF-IDF
-    new_user_vector = tfidf_model.transform([new_user_profile])
-
+    new_user_vector = tfidf.transform([new_user_profile])
     # Calcular la similitud con los usuarios existentes
     sim_scores = linear_kernel(new_user_vector, tfidf_matrix).flatten()
-
     # Obtener los índices de los usuarios más similares
     top_user_indices = sim_scores.argsort()[-10:][::-1]  # Top 10 usuarios similares
-
     # Devolver las recomendaciones
     return df['iid'].iloc[top_user_indices]
 
@@ -33,5 +30,5 @@ with st.form("my_form"):
     submitted = st.form_submit_button("Obtener Recomendaciones")
     if submitted:
         user_profile = f"{age} {race} {gender}"
-        recommendations = get_recommendations(user_profile, tfidf, tfidf, cosine_sim)
+        recommendations = get_recommendations(user_profile, tfidf, cosine_sim)
         st.write("Recomendaciones:", recommendations)
